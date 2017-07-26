4b7fbd13fd2cc6895a625600a8db33dc65f34962
Robert Jones <robert.g.jones@gmail.com>
2017-07-26 12:24:17 +0100

    Failing Feature Test: User makes a deposit
    The user makes a deposit to an Account object and is expected to receive
    confirmation of success to STDOUT. It asks for an Account class that is
    initialised with an integer balance.

---
+++ spec/features/user_makes_deposit_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe 'User makes a deposit' do
+  it 'expects balance increase confirmation' do
+    deposit = 1000
+    account = Account.new(0)
+    expect { account.make_deposit(deposit) }.to output("Deposit successful. Balance £#{balance}")
+  end
+end
+
